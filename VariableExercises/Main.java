b84ec25735173f6cd675fe5d5457bffca525c788
Ttack412 <31516693+Ttack412@users.noreply.github.com>
2022-02-23 19:44:52 -0500

    Add files via upload

---
+++ VariableExercises/Main.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void main(String[] args) {
+    // 1. Create an int variable with the value 10.
+    int myInt = 10;
+    
+    // 2. Print out this variable in a print statement.
+    System.out.println(myInt);
+
+    // 3. Create a string variable with the sentence "Hello World!"
+   String myString = "Hello World!";
+
+    // 4. Print the string.
+    System.out.println(myString);
+
+    // 5. Directly print the value 15 without a variable.
+    System.out.println(15);
+
+    // 6. Directly print the sentence "This is a string" without a variable
+    System.out.println("This is a string");
+  }
+}
